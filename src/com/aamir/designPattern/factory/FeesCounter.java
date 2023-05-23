b8c2ebc67631373f9fd33f923cff46f65c855a3a
mohammadaamir1102 <86870356+mohammadaamir1102@users.noreply.github.com>
2023-05-23 19:36:00 +0530

    factory design pattern

--- src/com/aamir/designPattern/factory/FeesCounter.java
+++ src/com/aamir/designPattern/factory/FeesCounter.java
@@ -5,8 +5,19 @@ public class FeesCounter {
     public static void main(String[] args) {
         College bcaDepartment = new BCADepartment();
         /*
-        * It is tightly couple class here creating the object  without input or requirement
-        */
+         * It is tightly couple class here creating the object  without input or requirement
+         */
+
+        College bcaDepartmentObject = CollegeFactory.getFeesDepartment("BCA Department");
+        System.out.println(bcaDepartmentObject != null ? bcaDepartmentObject.courseFees() : null);
+        System.out.println("+++++++++++++++++++");
+
+        College bTeachDepartmentObject = CollegeFactory.getFeesDepartment("BTeach Department");
+        System.out.println(bTeachDepartmentObject != null ? bTeachDepartmentObject.courseFees() : null);
+        System.out.println("++++++++++++++++++++");
+
+        College mbaDepartmentObject = CollegeFactory.getFeesDepartment("MBA Department");
+        System.out.println(mbaDepartmentObject != null ? mbaDepartmentObject.courseFees() : null);
 
     }
 }
