2a384fbc31db55c0c88340b0d4ed1914dd8d9c47
mohammadaamir1102 <mohammadaamir1102@gmail.com>
2023-10-15 21:46:23 +0530

    adapter design patter

---
+++ src/com/aamir/designPattern/adapter/AdapterMain.java
@@ -0,0 +1,15 @@
+package com.aamir.designPattern.adapter;
+
+public class AdapterMain {
+
+    /*
+    * Suppose I have iphone and my friend has android phone, and we all have diff diff charging cable
+    * and I forgot my iphone charger and here want to use my friend android charger
+    * so here this is done by adapter it's means iphone want to use the object of android
+    * but cannot use directly. It is done by adapter class.
+    *
+    */
+    public static void main(String[] args) {
+
+    }
+}
